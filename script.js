98b14a982efab0fc873baf10f1cd394ea3f2a911
SIDHANT SRIVASTAVA <167967793+sidhant213@users.noreply.github.com>
2025-03-26 22:04:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log(window)
+// let object=confirm('Are you human')
+// console.log(object)
+// let data=prompt('enter any data')
+// console.log(data)
+Name=prompt('Enter your name')
+console.log(Name)
+phoneNo=prompt('enter your phone number')
+console.log(phoneNo)
+gmail=prompt('enter a your gmail')
+console.log(gmail)
+address=prompt('enter your address')
+console.log(address)
+document.getElementById("name").innerHTML=Name
+document.getElementById("Mobile_No").innerHTML=phoneNo
+document.getElementById("G_mail").innerHTML=gmail
+document.getElementById("Address").innerHTML=address
